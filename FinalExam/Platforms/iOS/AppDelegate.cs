101182de1ff87a909aec6f0acd1792640bf310a3
180cms <yung7201913@gmail.com>
2024-01-12 13:50:48 +0800

    Add project files.

---
+++ FinalExam/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FinalExam
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
